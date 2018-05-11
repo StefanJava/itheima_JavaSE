a3a9cf69aa656066c065a3744a9838c5237cd3c7
stefan <923775370@qq.com>
2018-05-11 16:21:08 +0800

    Initial commit

---
+++ src/com/itheima/javase/Hello.java
@@ -0,0 +1,23 @@
+package com.itheima.javase;
+
+/**
+ * Created by 92377 on 2018/5/11
+ */
+public class Hello {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = a + 2;
+        System.out.println(b);
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        aa();
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+    }
+
+    public static void aa() {
+        System.out.println("aa");
+    }
+}
